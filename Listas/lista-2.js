af773629c74a0a4ea1bbcddc71a97aaa98ebaf05
gabi <gabriela.l.oliveira6@aluno.senai.br>
2025-01-29 11:00:22 -0300

    lista2

---
+++ Listas/lista-2.js
@@ -0,0 +1,51 @@
+//ex1
+let n1 = 17
+let n2 = 22
+
+let res1 = n1 > n2
+let res2 = n1 < n2
+let res3 = n1 == n2
+console.log("Ex 1: ", res1)
+console.log("Ex 1: ", res2)
+console.log("Ex 1: ", res3)
+
+//ex2
+let idade = 16
+
+let OpcaoVotar = idade >= 16
+let ObrigadoVotar = idade >= 18
+let ObrigadoVotar2 = idade <= 70
+console.log("Ex 2: ", OpcaoVotar)
+console.log("Ex 2: ", ObrigadoVotar)
+console.log("Ex 2: ", ObrigadoVotar2)
+
+//ex3
+let id = 19
+
+let MaiorId = id >= 18
+let Ido = id >= 60
+console.log("Ex 3: ", MaiorId)
+console.log("Ex 3: ", Ido)
+
+//ex4
+let NomeDeUsuario = "gabi" 
+
+let correto = NomeDeUsuario == "admin" 
+console.log("Ex 4: ", correto)
+
+//ex5
+let num = 25
+
+let div3 = num % 3 == 0
+let div5 = num % 5 == 0
+console.log("Ex 5: ", div3)
+console.log("Ex 5: ", div5)
+
+//ex6
+let saldo = 207
+let saque = 150
+
+let SaldoSuf = saque <= saldo
+let SaldoM = saldo - saque > 100
+console.log("EX 6: ", SaldoSuf)
+console.log("EX 6: ", SaldoM)
