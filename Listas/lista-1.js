b8804851469bc41a8b09bd7db251e7c62aac8b04
gabi <gabriela.l.oliveira6@aluno.senai.br>
2025-01-29 10:02:34 -0300

    update lista1

--- Listas/lista-1.js
+++ Listas/lista-1.js
@@ -23,3 +23,24 @@ let lar = 4
 
 let area = alt * lar
 console.log(area)
+
+//exercicio 3
+let num = 22
+
+let dob = num * 2
+let met = num / 2
+console.log("O dobro é: ", dob," A metade é: ", met)
+
+//exercicio 4
+let n1 = 7
+let n2 = 9
+let n3 = 8
+
+let media = (n1 + n2 + n3) / 3
+console.log(media)
+
+//exercicio 5
+let cel = 30
+
+let fah = (cel * 9/5) + 32
+console.log(fah)
