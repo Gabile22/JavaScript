79e6feccc1a6fc62dd4b903a04dd7e05379bf9a9
gabi <gabriela.l.oliveira6@aluno.senai.br>
2025-01-29 08:19:14 -0300

    Initial commit

---
+++ Listas/lista-1.js
@@ -0,0 +1,25 @@
+//exercicio 1
+let f = 8
+let g = 4
+
+let soma = f + g
+console.log(soma)
+
+let sub = f - g
+console.log(sub)
+
+let mult = f * g
+console.log(mult)
+
+let div = f / g
+console.log(div)
+
+let resto = f % g 
+console.log(resto)
+
+//exercicio 2
+let alt = 2
+let lar = 4
+
+let area = alt * lar
+console.log(area)
---
+++ Operadores/aritmeticos.js
@@ -0,0 +1,29 @@
+//console.log("Olá, Mundo!")
+
+//para declarar variáveis
+let nome = "Juliana"
+var nomes = "Juliana"
+
+//CTRL + ; -> COMENTARIOS
+console.log(nome)
+
+let idade = 24
+console.log("Minha idade é ", idade, "anos")
+//CONCATENCAENAÇÃO = juntar texto e valor
+
+
+//OPERAÇÕES MATEMÁTICAS
+let a = 3
+let b = 2
+
+let soma = a + b
+let sub = a - b
+let mult = a * b
+let div = a / b
+let resto = a % b //o que sobra da divião
+
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
+console.log(resto)
