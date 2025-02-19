7cf97174cff7c5e635ef6eaee490ba632a049bc7
gabi <gabriela.l.oliveira6@aluno.senai.br>
2025-02-19 10:05:46 -0300

    up

--- Listas/lista-10.js
+++ Listas/lista-10.js
@@ -30,24 +30,45 @@ let cid = ["Suzano", "Poá"]
 for(let i = 0; i < cid.length; i++){
     console.log(cid[i])
 }
+cid.unshift("Mogi")
+console.log(cid)
 
-// console.log("---------ex8--------")
-// let vazio = []
-// console.log("adiciona no final")
-// vazio.push("3", "2", "1")
-// console.log(vazio)
+console.log("---------ex6-------")
+let animal = ["papagaio", "gato", "peixe"]
+for(let i = 0; i < animal.length; i++) {
+    console.log(animal[i])
+    
+}
+animal.shift()
+console.log(animal)
 
-// console.log("adiciona ao início")
-// vazio.unshift("4")
-// console.log(vazio)
+console.log("---------ex7-------")
+let carros = ["civic", "fusca", "celta", "camaro"]
+for(let i = 0; i < carros.length; i++) {
+    console.log(carros[i])
+}
+console.log(carros)
 
-// console.log("remove o  último")
-// vazio.pop()
-// console.log(vazio)
+console.log("---------ex8--------")
+let vazio = []
+console.log("adiciona no final")
+vazio.push("3", "2", "1")
+console.log(vazio)
 
-// console.log("remove o primeiro")
-// vazio.unshift()
-// console.log(vazio)
+console.log("adiciona ao início")
+vazio.unshift("4")
+console.log(vazio)
 
-// console.log("array final")
-// for (let i = 0;)
+console.log("remove o  último")
+vazio.pop()
+console.log(vazio)
+
+console.log("remove o primeiro")
+vazio.shift()
+console.log(vazio)
+
+console.log("array final")
+for (let i = 0; i < vazio.length; i++){
+    console.log(vazio[i])
+}
+console.log(vazio)
