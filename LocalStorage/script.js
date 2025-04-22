0d380d6f9f32e166f08fc7088fa55c36c6086a4c
gabi <gabriela.l.oliveira6@aluno.senai.br>
2025-04-22 11:37:32 -0300

    up

---
+++ LocalStorage/script.js
@@ -0,0 +1,14 @@
+//QUANDO CARREGAR A PAGINA, MOSTRAR O QUE JÁ ESTAVA SALVO
+window.onload = function(){
+    const textoSalvo = localStorage.getItem("minhaAnotacao")
+    if(textoSalvo) {
+        document.getElementById("anotacao").value = textoSalvo
+    }
+}
+
+//SALVAR QUANDO CLICAR NO BOTÃO
+function salvarAnotacao(){
+    const conteudo = document.getElementById("anotacao").value
+    localStorage.setItem("minhaAnotacao", conteudo)
+    alert("Nota salva com sucesso")
+}
